ada1bfaabbbef92bc787fcf1207621a6b85058b5
StroginA <103519095+StroginA@users.noreply.github.com>
2022-10-24 23:52:01 +0300

    invite page

--- components/InviteScreen.vue
+++ components/InviteScreen.vue
@@ -1,29 +1,60 @@
 <template>
     <main class="invite">
-        <p class="invite__text">
-            You have {{isAdmin ? "successfully created" : "been invited to"}}
+        <p v-if="$fetchState.pending">
+            Please wait...
         </p>
-        <p class="invite__name">
-            {{sessionName}}
-        </p>
-        <p class="invite__text">
-            under the name
-        </p>
-        <p class="invite__name">
-            {{name}}
-        </p>
+        <template v-else-if="$fetchState.error">
+            <p>
+                There has been an error processing this invite code.
+                <br>
+                Perhaps it was copied incorrectly or was used already?
+            </p>
+            <Button class="invite__button" @click="$router.push('/')">Return to the main page</Button>
+        </template>
+        <template v-else>
+            <span class="invite__text">
+                You have {{inviteDetails?.isAdmin ? "successfully created" : "been invited to"}}
+            </span>
+            <span class="invite__name">
+                {{inviteDetails?.sessionName}}
+            </span>
+            <span class="invite__text">
+                under the name
+            </span>
+            <span class="invite__name">
+                {{inviteDetails?.name}}
+            </span>
+            <Button class="invite__button">Proceed</Button>
+        </template>
     </main>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import Button from '@/components/common/Button.vue'
+
 export default Vue.extend({
   name: "InviteScreen",
+  components: { Button },
   props: {
-    name: String,
-    isAdmin: Boolean,
-    sessionName: String
-  }
+    inviteCode: String
+  },
+  data() {
+    return {
+        inviteDetails: null as null | {name: string, isAdmin: boolean, sessionName: string}
+    }
+  },
+  async fetch() {
+    const {$axios} = this.$nuxt.context;
+    this.inviteDetails = await $axios.$get(
+        'api/users/getInviteDetails', {
+            params: {
+                inviteCode: this.inviteCode
+            }
+        }
+    ) as {name: string, isAdmin: boolean, sessionName: string};
+  },
+  fetchOnServer: false
 })
 </script>
 
@@ -32,14 +63,15 @@ export default Vue.extend({
     display: flex;
     flex-direction: column;
     align-items: center;
+    text-align: center;
     padding: 0.75rem;
 
     &__text {
-        text-align: center;
-        margin-bottom: 1.25rem;
+        margin-bottom: 1rem;
     }
     &__name {
-        font-size: 1.25rem;
+        font-size: 1.5rem;
+        margin-bottom: 1rem;
     }
 }
 </style>
--- pages/invite/_inviteCode/index.vue
+++ pages/invite/_inviteCode/index.vue
@@ -1,8 +1,6 @@
 <template>
     <InviteScreen
-    :name="inviteDetails.name"
-    :isAdmin="inviteDetails.isAdmin"
-    :sessionName="inviteDetails.sessionName"
+    :inviteCode="$route.params.inviteCode"
     />
 </template>
 
@@ -12,16 +10,6 @@ import InviteScreen from '@/components/InviteScreen.vue';
 
 export default Vue.extend({
     name: "IndexPage",
-    components: { InviteScreen },
-    async asyncData({params, $axios}) {
-        const inviteDetails = await $axios.$get(
-            'api/users/getInviteDetails', {
-                params: {
-                    inviteCode: params.inviteCode
-                }
-            }
-        ) as {name: string, isAdmin: boolean, sessionName: string};
-        return { inviteDetails }
-    }
+    components: { InviteScreen }
 })
 </script>
